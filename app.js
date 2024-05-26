d6f51170ba38ba53a5c5fa7cdde48f766734b749
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-05-26 17:36:51 +0100

    Add read file content functionality

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const fs = require('fs/promises');
 
 (async () => {
+    const commandFileHandler = await fs.open('./command.txt', 'r');
+
     const watcher = fs.watch('./command.txt');
 
     for await (const event of watcher) {
         if (event.eventType === 'change') {
-            // the file was changed...
-            console.log('The file was changed');
+            // get the size of the file
+            const size = (await commandFileHandler.stat()).size;
+            // allocaate buffer with the size of the file
+            const buff = Buffer.alloc(size);
+            // the location at which buffer should start filing
+            const offset = 0;
+            // how many bytes to read
+            const length = buff.byteLength;
+            // the position that reading the file should from
+            const position = 0;
+
+            // read the whole content of the file (from the beginning all the way to the end of the file.)
+            const content = await commandFileHandler.read(
+                buff,
+                offset,
+                length,
+                position
+            );
+            console.log(content);
         }
     }
 })();
