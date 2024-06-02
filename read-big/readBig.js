dff9379010658c3929253eb025b6263808c6da41
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-06-02 17:41:06 +0100

    Writing data from readable streams

--- read-big/readBig.js
+++ read-big/readBig.js
@@ -1,6 +1,7 @@
 const fs = require('node:fs/promises');
 
 (async () => {
+    console.time('readBig');
     const fileHandleRead = await fs.open('src.txt', 'r');
     const fileHandleWrite = await fs.open('dest.txt', 'w');
 
@@ -8,13 +9,39 @@ const fs = require('node:fs/promises');
 
     const streamWrite = fileHandleWrite.createWriteStream();
 
+    let split = '';
     streamRead.on('data', (chunk) => {
-        if (!streamWrite.write(chunk)) {
-            streamRead.pause();
+        const numbers = chunk.toString('utf-8').split('  '); // split at double spaces.
+
+        if (Number(numbers[0] !== Number(numbers[1] - 1))) {
+            if (split) numbers[0] = split.trim() + numbers[0].trim();
         }
+
+        if (
+            Number(numbers[numbers.length - 2]) + 1 !==
+            Number(numbers[numbers.length - 1])
+        ) {
+            split = numbers.pop();
+        }
+
+        numbers.forEach((number) => {
+            let n = Number(number);
+
+            // Check for even numbers
+            if (n % 2 === 0) {
+                if (!streamWrite.write(' ' + n + ' ')) {
+                    streamRead.pause();
+                }
+            }
+        });
     });
 
     streamWrite.on('drain', () => {
         streamRead.resume();
+    });
+
+    streamRead.on('end', () => {
+        console.log('Done reading!');
+        console.timeEnd('readBig');
     });
 })();
