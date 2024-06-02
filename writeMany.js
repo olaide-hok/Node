1b2bf2d043646ad65b7ebc280ae9510ecc569857
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-06-02 12:29:44 +0100

    Writing million times to a file without specifying character encoding.

---
+++ writeMany.js
@@ -0,0 +1,45 @@
+// const fs = require('node:fs/promises');
+
+// Execution Time: 2:24.633 (m:ss.mmm)
+// CPU Usage: (one core)
+// Memory Usage:
+// (async () => {
+//     console.time('writeMany');
+//     const fileHandle = await fs.open('test.txt', 'w');
+
+//     for (let i = 0; i < 1000000; i++) {
+//         await fileHandle.write(` ${i} `);
+//     }
+//     console.timeEnd('writeMany');
+// })();
+
+// Using callback approach.
+// const fs = require('node:fs');
+
+// (async () => {
+//     console.time('writeMany');
+//     fs.open('test.txt', 'w', (err, fd) => {
+//         for (let i = 0; i < 1000000; i++) {
+//             fs.write(fd, ` ${i} `, () => {});
+//         }
+
+//         console.timeEnd('writeMany');
+//     });
+// })();
+
+// Using callback approach.
+const fs = require('node:fs');
+
+// Execution Time: 00:15.323 (m:ss.mmm)
+// CPU Usage: (one core)
+// Memory Usage:
+(async () => {
+    console.time('writeMany');
+    fs.open('test.txt', 'w', (err, fd) => {
+        for (let i = 0; i < 1000000; i++) {
+            fs.writeSync(fd, ` ${i} `);
+        }
+
+        console.timeEnd('writeMany');
+    });
+})();
