33be49e0559e4ba4ca2947ae81971b1710ece476
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-06-08 17:50:36 +0100

    Streams with piping

--- copy/copy.js
+++ copy/copy.js
@@ -14,24 +14,42 @@ const fs = require('node:fs/promises');
 // Another approach using streams with Buffer.
 // Memory Usage:
 // Execution Time: 1.010s
+// (async () => {
+//     console.time('copy');
+//     const srcFile = await fs.open('src.txt', 'r');
+//     const destFile = await fs.open('src-copy.txt', 'w');
+
+//     let bytesRead = -1;
+//     while (bytesRead !== 0) {
+//         const readResult = await srcFile.read();
+//         bytesRead = readResult.bytesRead;
+
+//         if (bytesRead !== 16384) {
+//             const indexOfNotFilled = readResult.buffer.indexOf(0);
+//             const newBuffer = Buffer.alloc(indexOfNotFilled);
+//             readResult.buffer.copy(newBuffer, 0, 0, indexOfNotFilled);
+//             destFile.write(newBuffer);
+//         } else {
+//             destFile.write(readResult.buffer);
+//         }
+//     }
+//     console.timeEnd('copy');
+// })();
+
+// Another approach using streams with piping
+// Memory Usage:
+// Execution Time: 525.682ms
 (async () => {
     console.time('copy');
     const srcFile = await fs.open('src.txt', 'r');
     const destFile = await fs.open('src-copy.txt', 'w');
 
-    let bytesRead = -1;
-    while (bytesRead !== 0) {
-        const readResult = await srcFile.read();
-        bytesRead = readResult.bytesRead;
+    const readStream = srcFile.createReadStream();
+    const writeStream = destFile.createWriteStream();
 
-        if (bytesRead !== 16384) {
-            const indexOfNotFilled = readResult.buffer.indexOf(0);
-            const newBuffer = Buffer.alloc(indexOfNotFilled);
-            readResult.buffer.copy(newBuffer, 0, 0, indexOfNotFilled);
-            destFile.write(newBuffer);
-        } else {
-            destFile.write(readResult.buffer);
-        }
-    }
-    console.timeEnd('copy');
+    readStream.pipe(writeStream);
+
+    readStream.on('end', () => {
+        console.timeEnd('copy');
+    });
 })();
