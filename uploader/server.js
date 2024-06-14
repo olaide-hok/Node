dae0c60001dd1df9f3cba57ea3a203c9051808ed
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-06-14 18:35:17 +0100

    Get file names dynamically from CLI

--- uploader/server.js
+++ uploader/server.js
@@ -11,11 +11,17 @@ server.on('connection', (socket) => {
     socket.on('data', async (data) => {
         if (!fileHandle) {
             socket.pause(); // pause receiving data from the client
-            fileHandle = await fs.open(`storage/test.txt`, 'w');
+
+            const indexOfDivider = data.indexOf('-------');
+            const fileName = data
+                .subarray(10, indexOfDivider)
+                .toString('utf-8');
+
+            fileHandle = await fs.open(`storage/${fileName}`, 'w');
             fileWriteStream = fileHandle.createWriteStream(); // the stream to write to
 
             // Writing to our destination file, discard the headers
-            fileWriteStream.write(data);
+            fileWriteStream.write(data.subarray(indexOfDivider + 7));
 
             socket.resume(); // resume receiving data from the client
             fileWriteStream.on('drain', () => {
@@ -30,7 +36,7 @@ server.on('connection', (socket) => {
 
     // This end event happens when the client.js file ends the socket
     socket.on('end', () => {
-        fileHandle.close();
+        if (fileHandle) fileHandle.close();
         fileHandle = undefined;
         fileWriteStream = undefined;
         console.log('Connection ended!');
