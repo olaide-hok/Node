5bb6a6be5d9a149c2304a6d351ced5a2ecda0d67
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-06-14 18:12:12 +0100

    Handle back pressure during upload

--- uploader/server.js
+++ uploader/server.js
@@ -9,16 +9,31 @@ server.on('connection', (socket) => {
     console.log('New connection!');
 
     socket.on('data', async (data) => {
-        fileHandle = await fs.open(`storage/test.txt`, 'w');
-        fileWriteStream = fileHandle.createWriteStream();
+        if (!fileHandle) {
+            socket.pause(); // pause receiving data from the client
+            fileHandle = await fs.open(`storage/test.txt`, 'w');
+            fileWriteStream = fileHandle.createWriteStream(); // the stream to write to
 
-        // Writing to our destination file
-        fileWriteStream.write(data);
+            // Writing to our destination file, discard the headers
+            fileWriteStream.write(data);
+
+            socket.resume(); // resume receiving data from the client
+            fileWriteStream.on('drain', () => {
+                socket.resume();
+            });
+        } else {
+            if (!fileWriteStream.write(data)) {
+                socket.pause();
+            }
+        }
     });
 
+    // This end event happens when the client.js file ends the socket
     socket.on('end', () => {
-        console.log('Connection ended!');
         fileHandle.close();
+        fileHandle = undefined;
+        fileWriteStream = undefined;
+        console.log('Connection ended!');
     });
 });
 
