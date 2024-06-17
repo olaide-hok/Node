44e542dda44fc8a16dd0edd0ce76596c0fcdd65e
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-06-17 18:06:46 +0100

    Server file using custom Butter framework

---
+++ simple-web/server.js
@@ -0,0 +1,13 @@
+const Butter = require("./butter");
+
+const PORT = 4060;
+
+const server = new Butter();
+
+server.route("get", "/", (req, res) => {
+  res.sendFile("./public/index.html", "text/html");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
+});
